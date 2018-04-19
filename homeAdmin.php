12c0310be1a380ab0df895eb3436ed5023df29e0
hidayet <hidayet.bekkai@gmail.com>
2018-04-19 22:27:37 +0100

    login and registration

---
+++ homeAdmin.php
@@ -0,0 +1,8 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>HELLO ADMIN</h2>
+</body>
---
+++ verifInscription.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hp
+ * Date: 17/04/2018
+ * Time: 23:07
+ */
+
+
+$nomEtu = $_POST['nomEtu'];
+$prenomEtu = $_POST['prenomEtu'];
+$adresseEtu = $_POST['adresseEtu'];
+$anneeEtu = $_POST['anneeEtu'];
+$specialiteEtu = $_POST['specialiteEtu'];
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+$etudiant  = 'etudiant';
+
+try {
+
+    $connexionDB = new PDO('mysql:host=localhost;dbname=plan', 'root', '');
+    $connexionDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $insert = "INSERT INTO users(username, password, typeUser)
+      VALUES ('" . $username . "','" . $password . "', '". $etudiant ."')";
+    $connexionDB->exec($insert);
+    $idUser = $connexionDB->lastInsertId();
+
+    $insert = "INSERT INTO etudiants(nomEtu, prenomEtu, adresseEtu, anneeEtu, specialiteEtu, idUser)
+      VALUES ('" . $nomEtu . "','" . $prenomEtu . "','" . $adresseEtu . "','" . $anneeEtu . "','" . $specialiteEtu . "','" . $idUser . "')";
+    $connexionDB->exec($insert);
+
+
+    header("location:homeEtudiant.php");
+} catch
+(PDOException $e) {
+    die("Erreur: " . $e->getMessage());
+}
