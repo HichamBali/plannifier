676d3a08d93eb451275f7e8d7f383e6501070e45
hidayet <hidayet.bekkai@gmail.com>
2018-05-01 14:31:11 +0100

    récuperer liste de thèmes dans un tableau, dans la page choix

--- choix.php
+++ choix.php
@@ -81,6 +81,9 @@ else{
     </style>
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <!--Script sortable "liste fiche de voeux" -->
+
     <script>
         var datass;
 
@@ -103,12 +106,13 @@ else{
             });
         });
     </script>
+    <!-- Enregistrement data avec ajax et json -->
     <script>
         function savingdata(){
             $.ajax({
                 type: "POST",
                 dataType: "json",
-                url: "ajouterChoix.php", // ajouter a la bdd
+                url: "ajouterChoix.php", // ajouter à la bdd
                 data: {lists: datass},
                 success: function(result){
                     if(result.trim()=="success")
@@ -116,7 +120,6 @@ else{
                     $('#btnId').attr('disabled', 'true');// btn cliquable qu'une seule fois
                     document.getElementById('btnId').style.backgroundColor = "gray"; // changer couleur btn
                     document.getElementById('btnId').style.color = "white";
-
                 }
             });
         }
@@ -124,66 +127,113 @@ else{
     </script>
 </head>
 <body>
- <!-------------------------NAVBAR // à voir----------------------->
+<!-------------------------NAVBAR ----------------------->
 <div class="navbar-nav navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <a class="navbar-brand"><img src="images/plan.png" id="plan"> </a>
-        <a href="homeEtudiant.php" role="button"><i class="fa fa-level-up"></i> Retour</a> <!--pour le retour au home-->
-        <a href="logOut.php" role="button"><i class="fa fa-sign-out"></i> LogOut</a><!--pour déconnecter-->
+            <a href="homeEtudiant.php" role="button"><i class="fa fa-level-up"></i> Retour</a> <!--pour le retour au home-->
+            <a href="logOut.php" role="button"><i class="fa fa-sign-out"></i> LogOut</a><!--pour se déconnecter-->
         </div>
     </div>
 </div>
- <!-----------------------Afficher liste des thèmes avec details-------------->
+
+<!-----------------------Afficher liste des thèmes avec details-------------->
+<br/><br/>
+
+<h2 align="center" style="color: #2b542c"><u> Liste des thèmes proposés </u> </h2> <!--titre-->
 
 
+<div class="table-responsive" id="table">
+
+    <table id="tableTheme" role="grid" class="table table-striped table-bordered">
+
+        <!-- le head du tableau-->
+        <thead>
+        <tr>
+            <th id="idINFIRMIER" role="gridcell" style="display:none">ID</th>
+            <th id="nom_i" role="gridcell">Thème</th>
+            <th id="prenom_i" role="gridcell">Déscription</th>
+            <th id="adresse_i" role="gridcell">Encadreur</th>
 
 
+        </tr>
+        </thead>
+        <tbody>
+        <!-- on recupere les donnee du tab de la bdd -->
+        <?php
+        try{
+            $bdd = new PDO('mysql:host=localhost;dbname=plan;charset=utf8','root','') ;
+        }catch(Exception $e){
+            echo "errreur" ;
+        }
 
+        $r = $bdd->query('SELECT * FROM enseigants RIGHT JOIN themes ON 
+                                    themes.idEnseignant = enseigants.idEnseignant WHERE valide=1');
+        $row_count = 1;
+        while($donne=$r->fetch()){
+            ?>
+            <tr>
 
+                <td id="ids" role="gridcell" style="display:none"><?php echo $donne['idTheme'];?></td>
+                <td id="libelle" role="gridcell"><?php echo $donne['libelle'];?></td>
+                <td id="description" role="gridcell"><?php echo $donne['description'];?></td>
+                <td id="nomEns" role="gridcell"><?php echo $donne['nomEns'];?></td>
 
-
-
-
-
-
-
-
-
-     <!--------------Remplir fiche de voeux------------------>
-
-
-<h3 align="left">Liste des thèmes à choisir</h3>
-<ul id="sortable1">
-
-    <?php                    // on récupère les thèmes validés de la bdd
-    try {
-        $bdd = new PDO('mysql:host=localhost;dbname=plan;charset=utf8', 'root', '');
-    } catch (Exception $e) {
-        echo "erreur";
-    }
-
-    $r = $bdd->query('SELECT * FROM themes WHERE valide = 1'); // Tous les thèmes validés
-
-    $row_count = 1;
-    while ($donne = $r->fetch()) {
+            </tr>
+            <?php
+            $row_count ++ ;
+        }
         ?>
 
-        <li class="ui-state-default"  id="item_<?php echo $donne['idTheme'];?>" name="idTheme" ><?php echo $donne['libelle']; ?></li>
+        <tbody>
+    </table>
+</div>
+<!---------------------------texte Indication---------------------->
+<br/><br/>
+<h2 align="center" style="color: #2b542c"><u> Remplir ma fiche de voeux</u></h2>
+<br/>
+<h4 style="color: #953b39" align="center">Indication:</h4>
+<p style="color: #2b542c" align="center">
+    Pour faire remplir votre fiche de voeux
+    <br/> 1- faites glisser les les thèmes de la liste 1 au liste 2.
+    <br/>  2- Ordonner vox choix dan la liste 2.
+    <br/>  3- Cliquer sur envoyer ma fiche de voeux.
+    <br/> cette dernière étape ce fait qu'une seule fois, soyez sure de vox choix et ses classement. BON COURAGE!</p><br/>
+
+
+<!--------------Remplir fiche de voeux------------------>
+<div>
+    <ul id="sortable1">
+
+        <?php                    // on récupère les thèmes validés de la bdd
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=plan;charset=utf8', 'root', '');
+        } catch (Exception $e) {
+            echo "erreur";
+        }
+
+        $r = $bdd->query('SELECT * FROM themes WHERE valide = 1'); // Tous les thèmes validés
+
+        $row_count = 1;
+        while ($donne = $r->fetch()) {
+            ?>
+
+            <li class="ui-state-default"  id="item_<?php echo $donne['idTheme'];?>" name="idTheme" ><?php echo $donne['libelle']; ?></li>
 
 
 
-        <?php
-        $row_count++;
-    }
-    ?>
-</ul>
+            <?php
+            $row_count++;
+        }
+        ?>
+    </ul>
+    <ul id="sortable2" class="connectedSortable">
+        <h3 align="center" style="color: white">Glissez et ordonnez vos choix ici</h3>
+    </ul>
+    <button class="save" id= "btnId" onclick="savingdata()" style="color: white">Valider ma fiche de voeux</button>
 
-<ul id="sortable2" class="connectedSortable">
-    <h3 align="center" style="color: white">Glissez et ordonnez vos choix ici</h3>
-</ul>
-<button class="save" id= "btnId" onclick="savingdata()" style="color: white">Valider ma fiche de voeux</button>
-
+</div>
 
 </body>
 </html>
